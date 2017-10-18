377de629801e98731a58ff29f63e0802062467ad
Mentikora <notender_boy@ukr.net>
2017-10-19 02:59:39 +0300

    mainpage => change aside background

--- app/js/main.js
+++ app/js/main.js
@@ -6,6 +6,24 @@ $(document).ready(function (){
 	$('.menu__nav-categories-title').on('click', function (){
 		$(this).siblings('ul').stop().slideToggle(200);
 	});
-	
+
+	// change slides
+	if ( $('.section-categories') ){
+		var counter = 1;
+
+		setInterval( function(){
+			counter++;
+
+			$('aside').each(function(){
+				var data = $(this).data('background');
+
+				if( counter > 3){
+					counter = 1;
+				} 
+
+				$(this).css('background-image', 'url("images/' + data + counter + '.jpg")');
+			});
+		}, 10000);
+	}
 
 });
--- dist/js/main.js
+++ dist/js/main.js
@@ -1,1 +1,1 @@
-$(document).ready(function(){"use strict";$(".menu__nav-categories-title").on("click",function(){$(this).siblings("ul").stop().slideToggle(200)})});
+$(document).ready(function(){"use strict";if($(".menu__nav-categories-title").on("click",function(){$(this).siblings("ul").stop().slideToggle(200)}),$(".section-categories")){var i=1;setInterval(function(){i++,$("aside").each(function(){var e=$(this).data("background");i>3&&(i=1),$(this).css("background-image",'url("images/'+e+i+'.jpg")')})},1e4)}});
