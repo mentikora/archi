6c1f2535aa33a07b397aa11ce8828b0a6dfa01e5
Mentikora <notender_boy@ukr.net>
2018-01-17 02:03:03 +0200

    update

--- app/js/main.js
+++ app/js/main.js
@@ -1,3 +1,12 @@
+document.addEventListener("DOMContentLoaded", function(){
+	var welcomeSlide = $('.section-welcome');
+	var menu = $('#menu-wrapper');
+
+	if ( welcomeSlide.length ){
+		menu.addClass('hidden');
+	}
+});
+
 $(document).ready(function (){
 
 	'use strict';
@@ -5,6 +14,27 @@ $(document).ready(function (){
 	// show menu list
 	$('.menu__nav-categories-title').on('click', function (){
 		$(this).siblings('ul').stop().slideToggle(200);
+	});
+
+	// remove menu on welcome slide
+	var welcomeSlide = $('.section-welcome');
+	var menu = $('#menu-wrapper');
+	if ( welcomeSlide.length ){
+		$(window).scroll(function() {
+	    if ( $(this).scrollTop() < welcomeSlide.outerHeight() ) {
+	      menu.addClass('hidden');
+      }
+	    else {
+	      menu.removeClass('hidden');
+	    }
+	  });
+	}
+
+	// scroll bottom on welcome page button click
+	$('.section-welcome__menu-button').on('click', function(){
+		if (window.matchMedia("(min-width: 768px)").matches) {
+			$('html, body').animate( {scrollTop: $('.section-categories').offset().top }, 500);
+		}
 	});
 
 	// change slides
